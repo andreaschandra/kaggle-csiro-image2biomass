d4f93de2dd42b49573cb95697a931d524e3cdf24
Andreas Chandra <phoenix.ichigo@yahoo.com>
2025-12-25 11:00:01 +0700

    #2 add utils io and logger

---
+++ src/csiro_biomass/utils/logger.py
@@ -0,0 +1,38 @@
+"""Logging management."""
+
+import logging
+import os
+
+
+def setup_logger(name: str = "kaggle", level: int = logging.INFO) -> logging.Logger:
+    """
+    Sets up a logger with the specified name and logging level.
+
+    Args:
+        name (str): The name of the logger.
+        level (int): The logging level (default is logging.INFO).
+
+    Returns:
+        logging.Logger: Configured logger instance.
+    """
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+
+    if not logger.hasHandlers():
+
+        # add console handler
+        ch = logging.StreamHandler()
+        ch.setLevel(level)
+        ch.setFormatter(formatter)
+        logger.addHandler(ch)
+
+        # add file handler
+        if os.path.exists("./logs") is False:
+            os.makedirs("./logs")
+        fh = logging.FileHandler("./logs/app.log", mode="w")
+        fh.setLevel(level)
+        fh.setFormatter(formatter)
+        logger.addHandler(fh)
+
+    return logger
